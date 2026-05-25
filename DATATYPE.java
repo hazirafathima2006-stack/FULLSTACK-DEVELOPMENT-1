6664bca74a77484102b99a21281d74117db72d43
hazirafathima2006-stack <hazirafathima2006@gmail.com>
2026-05-25 09:56:14 +0530

    Create DATATYPE.java

---
+++ DATATYPE.java
@@ -0,0 +1,18 @@
+package DATATYPE;
+
+public class DATATYPE {
+	    public static void main(String[] args) {
+	        int age = 18;
+	        float height = 5.6f;
+	        double salary = 25000.75;
+	        char grade = 'A';
+	        boolean isStudent = true;
+	        String name =  "Hazira";
+	        System.out.println("Name : " + name);
+	        System.out.println("Age : " + age);
+	        System.out.println("Height : " + height);
+	        System.out.println("Salary : " + salary);
+	        System.out.println("Grade : " + grade);
+	        System.out.println("Student : " + isStudent);
+	    }
+	}
