2fc3ba1443c6f605d684ef47699bb28692b7a03e
hazirafathima2006-stack <hazirafathima2006@gmail.com>
2026-06-01 22:58:47 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,33 @@
+package MultiplicationTable;
+
+public class Table {
+	public static void main(String[] arg) {
+	 for (int i=1;i<=10;i++ ) { 
+		 for (int j = 2; j <= 2; j++) {
+	          System.out.println(j+ " * " + i + " = " + (j * i));
+ }	              
+	}
+	System.out.println();
+	 for (int i=1;i<=10;i++ ) { 
+		 for (int j = 3; j <= 3; j++) {
+	          System.out.println(j+ " * " + i + " = " + (j * i));
+ }	              
+	}
+	System.out.println();
+	 for (int i=1;i<=10;i++ ) { 
+		 for (int j = 4; j <= 4; j++) {
+	          System.out.println(j+ " * " + i + " = " + (j * i));
+ }	              
+	}
+	System.out.println();
+	 for (int i=1;i<=10;i++ ) { 
+		 for (int j = 5; j <= 5; j++) {
+	          System.out.println(j+ " * " + i + " = " + (j * i));
+ }	              
+	}
+	System.out.println();
+	
+}
+	
+	
+}
