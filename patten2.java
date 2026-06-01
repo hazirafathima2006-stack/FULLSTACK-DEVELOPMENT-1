d152a8facc272c5fc4ad3a06219763ba60f94cc5
hazirafathima2006-stack <hazirafathima2006@gmail.com>
2026-06-01 22:59:27 +0530

    Add files via upload

---
+++ patten2.java
@@ -0,0 +1,17 @@
+package patten;
+
+public class patten2 {
+	    public static void main(String[] args) {
+	        int rows = 5;
+
+	        for (int i = 1; i <= rows; i++) {
+	            char ch = 'A';
+
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(ch + " ");
+	                ch++;
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
---
+++ patten5.java
@@ -0,0 +1,10 @@
+package patten;
+
+public class patten5 {
+public static void main(String[ ] arg) {
+	for(int i=1;i<=5; i++) {
+		 System.out.println("*");
+	
+}
+}
+}
