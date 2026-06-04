7f6dbadf6ec413bacbacbb53937381b87ed7143b
hazirafathima2006-stack <hazirafathima2006@gmail.com>
2026-06-04 22:55:36 +0530

    Add files via upload

---
+++ Patten7.java
@@ -0,0 +1,18 @@
+package patten7;
+
+public class Patten7 {
+	public static void main(String[] arg) {
+		int n=5;
+		for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n-i; j++) {
+			System.out.print(" ");
+		}
+		for(int k=1; k<=(2*i-1); k++) {
+			System.out.print("*");
+		}
+		System.out.println("");	
+		
+		}
+		
+	}
+ }
